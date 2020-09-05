33f04b93a7ac41588d0cca94561886852f4e5126
Efrain Coello <carlosefrain17@hotmail.com>
2020-09-05 15:09:21 -0500

    JC-2 dynamodb connection

---
+++ server/test/libraries/test_dynamodb.py
@@ -0,0 +1,81 @@
+"""This module test dynamodb conection"""
+# pylint: disable=E1101, C0411, W0613, C0302, C0103
+from libraries import dynamodb
+
+
+def test_get_client(mocker):
+    """should have the connection"""
+    mocker.patch.object(dynamodb, 'boto3')
+    dynamodb.boto3.client.return_value = 'dynamodb client'
+    client = dynamodb.get_client('localhost', 'http://localhost:8000')
+    dynamodb.boto3.client.assert_called_with(
+        'dynamodb',
+        region_name='us-west-2',
+        endpoint_url='http://localhost:8000'
+    )
+    assert client == 'dynamodb client'
+
+
+def test_get_client_none(mocker):
+    """should have the connection"""
+    mocker.patch.object(dynamodb, 'boto3')
+    dynamodb.boto3.client.return_value = 'dynamodb client'
+    client = dynamodb.get_client('dev', 'http://localhost:8000')
+    dynamodb.boto3.client.assert_called_with(
+        'dynamodb',
+        region_name='us-west-2',
+        endpoint_url=None
+    )
+    assert client == 'dynamodb client'
+
+
+def test_get_resource(mocker):
+    """should have the resourse"""
+    mocker.patch.object(dynamodb, 'boto3')
+    dynamodb.boto3.resource.return_value = 'dynamodb resource'
+    resource = dynamodb.get_resource('localhost', 'http://localhost:8000')
+    dynamodb.boto3.resource.assert_called_with(
+        'dynamodb',
+        region_name='us-west-2',
+        endpoint_url='http://localhost:8000'
+    )
+    assert resource == 'dynamodb resource'
+
+
+def test_get_resource_none(mocker):
+    """should have the resourse"""
+    mocker.patch.object(dynamodb, 'boto3')
+    dynamodb.boto3.resource.return_value = 'dynamodb resource'
+    resource = dynamodb.get_resource('dev', 'http://localhost:8000')
+    dynamodb.boto3.resource.assert_called_with(
+        'dynamodb',
+        region_name='us-west-2',
+        endpoint_url=None
+    )
+    assert resource == 'dynamodb resource'
+
+
+def test_get_table(mocker):
+    """Should get table"""
+    params = {
+        'enviroment': 'localhost',
+        'table': 'JobsityChatTest',
+        'dynamodb_dns': 'http://localhost:8000'
+    }
+    mocker.patch.object(dynamodb, 'get_resource')
+    mocker.spy(MockTable, 'Table')
+    dynamodb.get_resource.return_value = MockTable
+    table = dynamodb.get_table(params)
+    # assert
+    assert table == 'JobsityChatTest'
+    MockTable.Table.assert_called_with('JobsityChatTest')
+    dynamodb.get_resource.assert_called_with('localhost', 'http://localhost:8000')
+
+
+class MockTable:
+    """docstring for MockTable"""
+
+    @staticmethod
+    def Table(table_name):
+        """docstring for MockTable"""
+        return table_name
