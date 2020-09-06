39d949ac9007e07f4c61150d722f8a4d0b2e72ed
Efrain Coello <carlosefrain17@hotmail.com>
2020-09-05 22:51:39 -0500

    JC-7 conection controller

--- server/controllers/conection.py
+++ server/controllers/conection.py
@@ -1,1 +1,20 @@
 """This module manage conection controller"""
+from libraries import hooks
+from models import conection as conection_mdl
+
+
+def insert_conection(event):
+    """Insert conection id"""
+    connection_id = event['requestContext']['connectionId']
+    new_conection = {
+        'connectionId': event['requestContext']['connectionId'], 
+        'chatRoom': 'chatRoom1',
+        'userEmail': hooks.SESSION['email']
+    }
+    conection_mdl.insert(new_conection)
+
+
+def delete_conection(event):
+    """delete conection id"""
+    connection_id = event['requestContext']['connectionId']
+    conection_mdl.delete(connection_id)
