8755e1ecf00945afe252eb36a4713c6e7d46f3b3
Efrain Coello <carlosefrain17@hotmail.com>
2020-09-06 23:44:02 -0500

    JC-15 get last messages new connection

--- server/jobsitychat/controllers/connection.py
+++ server/jobsitychat/controllers/connection.py
@@ -1,6 +1,8 @@
 """This module manage connection controller"""
+import json
 import boto3
 from jobsitychat.models import connection as connection_mdl
+from jobsitychat.models import message as message_mdl
 
 
 def insert_connection(event):
@@ -8,15 +10,38 @@ def insert_connection(event):
     access_token = event['queryStringParameters']['Authorizer']
     client = boto3.client('cognito-idp')
     user = client.get_user(AccessToken=access_token)
+    connection_id = event['requestContext']['connectionId']
     new_connection = {
-        'connectionId': event['requestContext']['connectionId'],
+        'connectionId': connection_id,
         'chatRoom': 'chatRoom1',
         'userName': user['Username']
     }
     connection_mdl.insert(new_connection)
+    send_last_mesages(event, connection_id)
 
 
 def delete_connection(event):
     """delete connection id"""
     connection_id = event['requestContext']['connectionId']
     connection_mdl.delete(connection_id)
+
+
+def send_last_mesages(event, connection_id):
+    """Send last 50 messages"""
+    messages = message_mdl.get_all_last()
+    domain = event['requestContext']['domainName']
+    stage = event['requestContext']['stage']
+    endpoint_url = f'https://{domain}/{stage}'
+    api_gateway = boto3.client(
+        'apigatewaymanagementapi', 
+        endpoint_url=endpoint_url
+    )
+    for message in messages:
+        message_data = {
+            'message': message['message'],
+            'userName': message['userName']
+        }
+        api_gateway.post_to_connection(
+            Data=json.dumps(message_data),
+            ConnectionId=connection_id
+        )
